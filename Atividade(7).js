0ec6d92db253c9ca686b05b309866c802c925566
EplayerEpic <114813889+EplayerEpic@users.noreply.github.com>
2022-11-19 10:52:58 -0300

    Add files via upload

---
+++ Atividade(7).js
@@ -0,0 +1,20 @@
+let x = "Ano atual 2022";
+x = 2022;
+
+alert(x)
+
+let y = prompt("Digite seu ano de nascimento"); 
+
+let Nome = prompt("Digite seu nome aqui"); 
+
+let z =  x - y
+
+document.write("Olá,seu nome é "+ Nome +"<br>" + "E você tem" + ' ' +  z + " Anos ")
+
+
+
+ 
+
+
+
+
